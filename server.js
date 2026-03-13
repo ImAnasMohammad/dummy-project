5cf704084d8512f2cfe796b961a7bec69367255d
Shaik Mohammad Anas <shaik_mohammadanas@epam.com>
2026-03-13 12:52:20 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.end("Hello world")
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server started successfully")
+})
